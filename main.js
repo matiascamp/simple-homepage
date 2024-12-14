b3eccb4fcd4ef197035c879a20a8cbc2f3021e7e
matias <you@example.com>
2024-12-14 14:56:45 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const sun = document.getElementsByClassName("sun");
+const moon = document.getElementsByClassName("moon");
+const logo = document.getElementById("logo");
+
+const toggleDesktop = document.getElementById("dark-mode-toggle");
+const toggleMobile = document.getElementById("dark-mode-toggle-mobile");
+
+
+const toggleDarkMode = (toggleElement) => {
+  const isChecked = toggleElement.checked
+  
+  document.body.classList.toggle("dark-mode", isChecked);
+  console.log("toggle checkec", isChecked);
+  console.log("  moon.style.filter",  moon);
+
+  for (let i = 0; i < moon.length; i++) {
+    moon[i].style.filter = isChecked ? "brightness(1) invert(0)" : "brightness(0) invert(1)"
+  }
+  for (let i = 0; i < sun.length; i++) {
+    sun[i].style.filter = isChecked ? "brightness(0) invert(1)"  :"brightness(1) invert(0)"
+  }
+
+  
+  logo.src = isChecked
+    ? "./assets/alarado-icon-homepage-dark.svg"
+    : "./assets/alarado-icon-homepage.svg";
+}
+
+
+toggleDesktop.addEventListener("change", () => toggleDarkMode(toggleDesktop));
+toggleMobile.addEventListener("change", () => toggleDarkMode(toggleMobile));
+
+const mediaQuery = window.matchMedia("(max-width: 640px)");
+
+function handleScreenChange(e) {
+  if (e.matches) {
+    const aside = document.querySelector("aside");
+    const openButton = document.getElementById("menu-toggle");
+    const closeButton = document.getElementById("accordion-toggle");
+
+    openButton.addEventListener("change", () => {
+      if (openButton.checked) {
+        aside.classList.add("open");
+        closeButton.checked = false
+      }
+    });
+
+
+    if (closeButton) {
+      closeButton.addEventListener("change", () => {
+        if (closeButton.checked) {
+          aside.classList.remove("open");
+          openButton.checked = false
+        }
+      });
+    }
+
+  }
+}
+
+// Escucha los cambios en el tamaño de la pantalla
+mediaQuery.addEventListener("change", handleScreenChange);
+
+// Llama a la función una vez al cargar la página
+handleScreenChange(mediaQuery);
